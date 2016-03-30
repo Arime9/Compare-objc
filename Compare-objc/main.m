9742c68e61b7f9e46996da598c3fdb8ce1290eae
Arai <arai@yazashin.local>
2016-03-30 11:42:58 +0900

    init Project

---
+++ Compare-objc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Compare-objc
+//
+//  Created by Arai on 2016/03/30.
+//  Copyright © 2016年 masato_arai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
